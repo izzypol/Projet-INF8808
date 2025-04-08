7b856ec3fdcb9a69709531233bed1a35a0a36df4
Alexndrs <alexandred56700@gmail.com>
2025-04-08 14:46:04 -0400

    Supprime les dossiers suivis de Git pour respecter .gitignore

--- code/dist/src.e31bb0bc.js
+++ code/dist/src.e31bb0bc.js
@@ -1381,7 +1381,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54104" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55200" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
